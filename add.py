a82fbcea3586adb045ba673c4be76d9527115d2c
somsekhar <vijayadurgab78@gmail.com>
2024-01-11 14:05:00 +0530

    this is version1 file

---
+++ add.py
@@ -0,0 +1,6 @@
+this is a c program python filr
+int sum1,sum2,sum;
+scanf &sum1,&sum2
+printf enter values of sum1 and sum2
+sum=sum1+sum2[A[C[C[C[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[e[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+printf sum
